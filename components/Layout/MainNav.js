876dcd888ad8c20b3e2478aa2cc493a99ead2b5c
AGB117 <abner.gonzalez4@gmail.com>
2023-06-05 14:42:37 -0400

    mobile test

--- components/Layout/MainNav.js
+++ components/Layout/MainNav.js
@@ -60,11 +60,13 @@ function MainNav(props) {
       document.body.style.backgroundImage = "url('space.webp')";
       document.body.style.color = "#fff";
       document.body.style.backgroundSize = "cover";
+      document.body.style.backgroundRepeat = "repeat";
     } else if (!spaceTheme && mobileNav) {
       document.body.style.fontFamily = "Georgia, serif";
       document.body.style.backgroundImage = "url('background.webp')";
       document.body.style.color = "#333";
       document.body.style.backgroundSize = "cover";
+      document.body.style.backgroundRepeat = "repeat";
     }
     console.log(spaceTheme);
     //desktop
