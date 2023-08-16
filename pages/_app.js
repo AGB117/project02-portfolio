46d494e081b5d4670a0fc0eded7892c940e6d17e
AGB117 <abner.gonzalez4@gmail.com>
2023-08-16 11:08:34 -0400

    added analytics for Vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import Layout from "../components/Layout/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
